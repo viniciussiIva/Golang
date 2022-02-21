5f7c712d78fa287cb43ee6b03d4214c788eb0a5b
Vinícius Melo <viniciusvsom@gmail.com>
2022-02-21 11:39:36 -0800

    Enviando arquivos referente a aula de variaveis

---
+++ 02 - Variáveis/variaveis.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "variavel_1"
+	variavel2 := "variavel_2"
+
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3  string = "teste_variável_3"
+		vaariavel4 string = "teste_variável_4"
+	)
+
+	fmt.Println(variavel3, vaariavel4)
+
+	variavel5, variavel6 := "variável_5", "variável_6"
+
+	fmt.Println(variavel5, variavel6)
+}
