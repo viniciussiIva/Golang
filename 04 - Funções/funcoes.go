9ac0c70781b8d645266ea3dcf2b2924e09e095d3
Vinícius Silva <viniciusvsom@gmail.com>
2022-02-28 18:54:25 -0300

    Update funcoes.go

--- 04 - Funções/funcoes.go
+++ 04 - Funções/funcoes.go
@@ -11,6 +11,11 @@ func calculosMatematicos(n1, n2 int8) (int8, int8) {
 	subtracao := n1 - n2
 	return soma, subtracao
 }
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
 
 func main() {
 	soma := somar(10, 13)
