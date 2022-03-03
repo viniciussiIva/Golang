35f7cb060e3d556b8841a949f95dfa7897519688
Vinícius Melo <viniciusvsom@gmail.com>
2022-03-03 11:27:49 -0300

    Arquivo relacionado a switch

---
+++ 12 - Switch/switch.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func dia_da_semana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-Feira"
+	case 3:
+		return "Terceira-Feira"
+	case 4:
+		return "Quarta-Feira"
+	case 5:
+		return "Quinta-Feira"
+	case 6:
+		return "Sexta-Feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+}
+
+func dia_da_semana_2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda-Feira"
+	case numero == 3:
+		return "Terça-Feira"
+	case numero == 4:
+		return "Quarta-Feira"
+	case numero == 5:
+		return "Quinta-Feira"
+	case numero == 6:
+		return "Sexta-Feira"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+}
+
+func main() {
+
+	dia := dia_da_semana(2)
+	fmt.Println(dia)
+
+	dia2 := dia_da_semana_2(5)
+	fmt.Println(dia2)
+}
