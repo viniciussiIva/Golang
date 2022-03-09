03d19199f48ba71294b28c32e169777b634a6500
Vinícius Melo <viniciusvsom@gmail.com>
2022-03-09 15:16:56 -0300

    Alteraçoes funcoes

--- 13 - Loops/loops.go
+++ 13 - Loops/loops.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
 	i := 0
 	for i < 10 {
-	i++
+		i++
 		fmt.Println("Incrementando", i)
 		time.Sleep(time.Second)
-	 }
+	}
 
-	 fmt.Println(i)
+	fmt.Println(i)
 
 	for j := 0; j < 10; j++ {
-	 	fmt.Println("Incrometando", j)
+		fmt.Println("Incrometando", j)
 		time.Sleep(time.Second)
 
-	 }
+	}
 
 	nomes := [3]string{"João", "Davi", "Lucas"}
 	for indice, nome := range nomes {
