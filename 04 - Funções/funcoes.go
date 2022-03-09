9ac34bb1eac35ab732c3b12b833e883ae142751a
Vinícius Melo <viniciusvsom@gmail.com>
2022-03-09 15:14:41 -0300

    Alteraçoes funcoes

--- 04 - Funções/funcoes.go
+++ 04 - Funções/funcoes.go
@@ -6,7 +6,7 @@ func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
 
-func calculosMatematicos(n1, n2 int8) (int8, int8) {
+func calculosMatematicos1(n1, n2 int8) (int8, int8) {
 	soma := n1 + n2
 	subtracao := n1 - n2
 	return soma, subtracao
