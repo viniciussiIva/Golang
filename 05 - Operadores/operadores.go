d5795a8338c52b77572d194aca41ac44832070c4
Vinícius Silva <viniciusvsom@gmail.com>
2022-03-29 16:39:52 -0300

    Update operadores.go

--- 05 - Operadores/operadores.go
+++ 05 - Operadores/operadores.go
@@ -18,15 +18,15 @@ func main() {
 	soma2 := numero1 + numero2
 	fmt.Println(soma2)
 
-	//FIM ARITMETICOS
+	
 
 	var variavel1 string = "String"
 	variavel2 := "String2"
 	fmt.Println(variavel1, variavel2)
 
-	//FIM DOS OPERADORES ARITMETICOS
+	
 
-	//OPERADORES RELACIONAIS
+	
 	fmt.Println(1 > 2)
 	fmt.Println(1 >= 2)
 	fmt.Println(1 == 2)
@@ -34,17 +34,17 @@ func main() {
 	fmt.Println(1 > 2)
 	fmt.Println(1 < 2)
 	fmt.Println(1 != 2)
-	//FIM DOS RELACIONAIS
+	
 
-	//OPERADORES LÓGICOS
+	
 	fmt.Println(".............")
 	verdadeiro, falso := true, false
 	fmt.Println(verdadeiro && falso)
 	fmt.Println(verdadeiro || falso)
 	fmt.Println(!verdadeiro)
-	//FIM OPERADORES LÓGICOS
+	
 
-	//OPERADORES UNÁRIOS
+	
 	numero := 10
 	numero++
 	numero += 15
@@ -53,6 +53,6 @@ func main() {
 	numero /= 10
 	numero %= 3
 	fmt.Println(numero)
-	//  FIM DOS OPESERADORES UNÁRIOS
+
 
 }
